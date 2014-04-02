aea37776bb74d677636b84e726d1b4fcccf26006
walt91 <walsalas_91@hotmail.com>
2014-04-01 19:26:51 -0600

    Tird Commit

---
+++ ProyectoPlugin/Models/PluginDb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoPlugin.Models
+{
+    public class PluginDb : DbContext
+    {
+        public PluginDb()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+}
